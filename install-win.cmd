f4fd9a1a5c1e3132593ced1ce8467320fb493466
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-08-31 05:54:23 +0300

    feat: add --output, --name, --iso, --help arguments

--- install-win.cmd
+++ install-win.cmd
@@ -11,58 +11,55 @@ set /A FREE_SPACE_THRESHOLD_MB=40960
 rem Installation conf variables
 set "VIRTUAL_BOX_SRC=%VBOX_MSI_INSTALL_PATH%"
 set "ADDITIONS_PATH=%VBOX_MSI_INSTALL_PATH%VBoxGuestAdditions.iso"
-set "MACHINE_NAME=Ubuntu 24-04 (OSC)"
+set "machine_name=Ubuntu 24-04 (OSC)"
 set "machine_dest=%UserProfile%\VirtualBox VMs"
 set "iso_src=.\ubuntu-24.04-desktop-amd64-autoinstall.iso"
 set /A machine_physical_cores=2
 set /A machine_ram_mb=2096
 set /A machine_disk_size_mb=25000
 
-call :validate_specs
-
-if !errorlevel! neq 0 (
-    call :log "Found 1 or more errors, exiting..." panic
-) else (
+call :parse_arguments %* && call :validate_specs
+if !errorlevel! equ 0 (
     rem Check if a machine with the given name already exists
-    VBoxManage showvminfo "%MACHINE_NAME%" > nul 2>&1
+    VBoxManage showvminfo "!machine_name!" > nul 2>&1
 
     if !errorlevel! equ 0 (
-        echo Found "%MACHINE_NAME%".
+        echo Found "!machine_name!".
     ) else (
-        echo Creating "%MACHINE_NAME%", "%machine_dest%\%MACHINE_NAME%".
+        echo Creating "!machine_name!", "!machine_dest!\!machine_name!".
 
         rem Create and register VM
         VBoxManage createvm ^
-            --name "%MACHINE_NAME%" ^
+            --name "!machine_name!" ^
             --ostype Ubuntu_64 ^
             --register ^
-            --basefolder "%machine_dest%"
+            --basefolder "!machine_dest!"
 
         echo Creating Disk Controllers.
 
         rem Create SATA controller
-        VBoxManage createhd --filename "%machine_dest%\%MACHINE_NAME%\%MACHINE_NAME%.vdi" --size %machine_disk_size_mb% --format VDI
-        VBoxManage storagectl "%MACHINE_NAME%" --name "SATA Controller" --add sata --controller IntelAhci
-        VBoxManage storageattach "%MACHINE_NAME%" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "%machine_dest%\%MACHINE_NAME%\%MACHINE_NAME%.vdi"
+        VBoxManage createhd --filename "!machine_dest!\!machine_name!\!machine_name!.vdi" --size %machine_disk_size_mb% --format VDI
+        VBoxManage storagectl "!machine_name!" --name "SATA Controller" --add sata --controller IntelAhci
+        VBoxManage storageattach "!machine_name!" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "!machine_dest!\!machine_name!\!machine_name!.vdi"
 
         rem Create IDE controller
-        VBoxManage storagectl "%MACHINE_NAME%" --name "IDE Controller" --add ide --controller PIIX4
-        VBoxManage modifyvm "%MACHINE_NAME%" --boot1 dvd --boot2 disk --boot3 none --boot4 none
+        VBoxManage storagectl "!machine_name!" --name "IDE Controller" --add ide --controller PIIX4
+        VBoxManage modifyvm "!machine_name!" --boot1 dvd --boot2 disk --boot3 none --boot4 none
     )
 
-    echo Configuring "%MACHINE_NAME%".
+    echo Configuring "!machine_name!".
 
     rem Configure CPU, Memory and VRAM
-    VBoxManage modifyvm "%MACHINE_NAME%" --ioapic on
-    VBoxManage modifyvm "%MACHINE_NAME%" --cpus %machine_physical_cores%
-    VBoxManage modifyvm "%MACHINE_NAME%" --memory %machine_ram_mb% --vram 128
+    VBoxManage modifyvm "!machine_name!" --ioapic on
+    VBoxManage modifyvm "!machine_name!" --cpus %machine_physical_cores%
+    VBoxManage modifyvm "!machine_name!" --memory %machine_ram_mb% --vram 128
 
-    echo Starting unattended installation of "%MACHINE_NAME%".
+    echo Starting unattended installation of "!machine_name!".
 
     rem Install OS with guest additions
     rem NOTE: VBoxManager unattended configure parameters are ignored, Ubuntu does not use 'DebianInstallerPreseed'
-    VBoxManage unattended install "%MACHINE_NAME%" ^
-        --iso="%iso_src%" ^
+    VBoxManage unattended install "!machine_name!" ^
+        --iso="!iso_src!" ^
         --additions-iso="%ADDITIONS_PATH%" ^
         --extra-install-kernel-parameters="quiet autoinstall ds=nocloud\;s=/cdrom/desktop" ^
         --install-additions ^
@@ -98,8 +95,33 @@ if /I "%2"=="PASS" (
 endlocal
 goto :eof
 
+:parse_arguments
+:loop
+if NOT "%1"=="" (
+    if "%1"=="--output" (
+        shift
+        call set "machine_dest=%%~1"
+    ) else if "%1"=="--iso" (
+        shift
+        call set "iso_src=%%~1"
+    ) else if "%1"=="--name" (
+        shift
+        call set "machine_name=%%~1"
+    ) else if "%1"=="--help" (
+        call :help
+        exit /b 1
+    ) else (
+        call :log "unkown flag '%1'." fail
+        call :help
+        exit /b 2
+    )
+
+    shift
+    goto :loop
+)
+goto :eof
+
 :validate_specs
-setlocal
 set /a error=0
 
 echo Starting validation.
@@ -110,11 +132,14 @@ for /F "tokens=2 delims==" %%a in ('wmic cpu get NumberOfCores /value') do set C
 rem Querying RAM size in bytes
 for /F "tokens=2 delims==" %%a in ('wmic ComputerSystem get TotalPhysicalMemory /value') do set RAM_SIZE_BYTES=%%a
 
+rem Get machine_dest drive letter
+for %%l in (!machine_dest!) do set dest_drive_letter=%%~dl
+
 rem Get the free space of C drive in bytes using WMIC
-for /F "tokens=2 delims==" %%a in ('wmic logicaldisk where "DeviceID='C:'" get FreeSpace /value') do set C_FREE_SPACE_BYTES=%%a
+for /F "tokens=2 delims==" %%a in ('wmic logicaldisk where "DeviceID='%dest_drive_letter%'" get FreeSpace /value') do set DEST_FREE_SPACE_BYTES=%%a
 
 set /A RAM_SIZE_MB=%RAM_SIZE_BYTES:~0,-4% / (1049)
-set /A C_FREE_SPACE_MB=%C_FREE_SPACE_BYTES:~0,-4% / (1049)
+set /A DEST_FREE_SPACE_MB=%DEST_FREE_SPACE_BYTES:~0,-4% / (1049)
 
 rem Check for VirtualBox
 if "%VIRTUAL_BOX_SRC%"=="" (
@@ -139,11 +164,11 @@ if %RAM_SIZE_MB% lss %RAM_THRESHOLD_MB% (
     call :log "Found '%RAM_SIZE_MB% MB'. REQUIRE: '%RAM_THRESHOLD_MB% MB or more'." pass
 )
 
-if %C_FREE_SPACE_MB% lss %FREE_SPACE_THRESHOLD_MB% (
-    call :log "C drive has less than '%FREE_SPACE_THRESHOLD_MB% MB' of free space, available:'%C_FREE_SPACE_MB% MB'." fail
+if %DEST_FREE_SPACE_MB% lss %FREE_SPACE_THRESHOLD_MB% (
+    call :log "%dest_drive_letter% drive has less than '%FREE_SPACE_THRESHOLD_MB% MB' of free space, available:'%DEST_FREE_SPACE_MB% MB'." fail
     set /a error=1
 ) else (
-    call :log "Found '%C_FREE_SPACE_MB% MB' free in C drive. REQUIRE: '%FREE_SPACE_THRESHOLD_MB% MB or more'." pass
+    call :log "Found '%DEST_FREE_SPACE_MB% MB' free in %dest_drive_letter% drive. REQUIRE: '%FREE_SPACE_THRESHOLD_MB% MB or more'." pass
 )
 
 exit /b !error!
